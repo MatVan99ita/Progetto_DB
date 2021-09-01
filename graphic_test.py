330e252e53ae533b518db20e5d31f9e2848670ce
MatVan99ita <matteo.vanni2@studio.unibo.it>
2021-09-01 18:50:20 +0200

    test grafici

---
+++ graphic_test.py
@@ -0,0 +1,75 @@
+
+import tkinter as tk
+root = tk.Tk()
+
+window_main = tk.Tk()
+window_main.title("ChallengeUP Games")
+screen_width = window_main.winfo_screenwidth()
+screen_height = window_main.winfo_screenheight()
+
+top_welcome_frame= tk.Frame(window_main)
+lbl_name = tk.Label(top_welcome_frame, text = "Name:")
+lbl_name.pack(side=tk.LEFT)
+ent_name = tk.Entry(top_welcome_frame)
+ent_name.pack(side=tk.LEFT)
+btn_Admin = tk.Button(top_welcome_frame, text="Connect", command=lambda : main())
+btn_User = tk.Button(top_welcome_frame, text="Connect", command=lambda : main())
+btn_Admin.pack(side=tk.LEFT)
+btn_User.pack(side=tk.RIGHT)
+top_welcome_frame.pack(side=tk.TOP)
+
+top_message_frame = tk.Frame(window_main)
+lbl_line = tk.Label(top_message_frame, text="***********************************************************").pack()
+lbl_welcome = tk.Label(top_message_frame, text="")
+lbl_welcome.pack()
+lbl_line_server = tk.Label(top_message_frame, text="***********************************************************")
+lbl_line_server.pack_forget()
+top_message_frame.pack(side=tk.TOP)
+
+
+top_frame = tk.Frame(window_main)
+top_left_frame = tk.Frame(top_frame, highlightbackground="green", highlightcolor="green", highlightthickness=1)
+lbl_your_name = tk.Label(top_left_frame, text="Your name: ", font = "Helvetica 13 bold")
+lbl_opponent_name = tk.Label(top_left_frame, text="Opponent: ")
+lbl_your_name.grid(row=0, column=0, padx=5, pady=8)
+lbl_opponent_name.grid(row=1, column=0, padx=5, pady=8)
+top_left_frame.pack(side=tk.LEFT, padx=(10, 10))
+
+
+top_right_frame = tk.Frame(top_frame, highlightbackground="green", highlightcolor="green", highlightthickness=1)
+lbl_game_round = tk.Label(top_right_frame, text="Game round (x) starts in", foreground="blue", font = "Helvetica 14 bold")
+lbl_timer = tk.Label(top_right_frame, text=" ", font = "Helvetica 24 bold", foreground="blue")
+lbl_game_round.grid(row=0, column=0, padx=5, pady=5)
+lbl_timer.grid(row=1, column=0, padx=5, pady=5)
+top_right_frame.pack(side=tk.RIGHT, padx=(10, 10))
+
+top_frame.pack_forget()
+
+middle_frame = tk.Frame(window_main)
+
+lbl_line = tk.Label(middle_frame, text="***********************************************************").pack()
+lbl_line = tk.Label(middle_frame, text="**** GAME LOG ****", font = "Helvetica 13 bold", foreground="blue").pack()
+lbl_line = tk.Label(middle_frame, text="***********************************************************").pack()
+
+round_frame = tk.Frame(middle_frame)
+lbl_round = tk.Label(round_frame, text="Round")
+lbl_round.pack()
+lbl_your_choice = tk.Label(round_frame, text="Your choice: " + "None", font = "Helvetica 13 bold")
+lbl_your_choice.pack()
+lbl_opponent_choice = tk.Label(round_frame, text="Opponent choice: " + "None")
+lbl_opponent_choice.pack()
+lbl_result = tk.Label(round_frame, text=" ", foreground="blue", font = "Helvetica 14 bold")
+lbl_result.pack()
+round_frame.pack(side=tk.TOP)
+
+final_frame = tk.Frame(middle_frame)
+lbl_line = tk.Label(final_frame, text="***********************************************************").pack()
+lbl_final_result = tk.Label(final_frame, text=" ", font = "Helvetica 13 bold", foreground="blue")
+lbl_final_result.pack()
+lbl_line = tk.Label(final_frame, text="***********************************************************").pack()
+final_frame.pack(side=tk.TOP)
+
+middle_frame.pack_forget()
+
+
+window_main.mainloop()
